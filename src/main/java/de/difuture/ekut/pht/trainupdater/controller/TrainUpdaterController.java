09babab59ae68ad232b118d07389181f18aebc3a
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-04-17 14:53:54 +0200

    [UPDATE] Update to new TrainAvailable message

--- src/main/java/de/difuture/ekut/pht/trainupdater/controller/TrainUpdaterController.java
+++ src/main/java/de/difuture/ekut/pht/trainupdater/controller/TrainUpdaterController.java
@@ -2,13 +2,13 @@ package de.difuture.ekut.pht.trainupdater.controller;
 
 
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.UUID;
 
 import de.difuture.ekut.pht.lib.core.dockerevent.DockerRegistryEvent;
 import de.difuture.ekut.pht.lib.core.dockerevent.DockerRegistryEventIterable;
 import de.difuture.ekut.pht.lib.core.messages.TrainAvailable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.support.MessageBuilder;
@@ -20,33 +20,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 
-import de.difuture.ekut.pht.trainupdater.message.TrainUpdateStreams;
-import lombok.NonNull;
-
 
 @RestController
 public class TrainUpdaterController {
 
 	private static final ResponseEntity<?> OK = ResponseEntity.ok().build();
-	private final TrainUpdateStreams trainUpdateStreams;
-	
-	@Autowired
-	public TrainUpdaterController(
-			@NonNull final TrainUpdateStreams trainUpdateStreams) {
 
-		this.trainUpdateStreams = trainUpdateStreams;
+	// Source for publishing messages
+	private final Source source;
+
+	@Autowired
+	public TrainUpdaterController(Source source) {
+
+		this.source = source;
 	}
-	
+
 	private boolean broadcastTrainAvailable(
 			final UUID trainID,
 			final URI trainRegistryURI,
-			final URI pusherURI) {
+			final String tag) {
 
-		return this.trainUpdateStreams.outboundTrainAvailable().send(
+		return this.source.output().send(
 				MessageBuilder
-					.withPayload(new TrainAvailable(trainID, trainRegistryURI, pusherURI))
-					.setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON)
-					.build());
+						.withPayload(new TrainAvailable(trainID, trainRegistryURI, tag))
+						.setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON)
+						.build()
+		);
 	}
 
 
@@ -56,20 +55,21 @@ public class TrainUpdaterController {
 		for (final DockerRegistryEvent event: events) {
 
 			final DockerRegistryEvent.Target target = event.getTarget();
+			final String tag = target.getTag();
 
 			try {
                 // Sends a train available message if
                 // * Docker Registry Event Action is Push
                 // * The tag is not null
-                if (event.getAction() == DockerRegistryEvent.Action.PUSH && target.getTag() != null) {
+                if (event.getAction() == DockerRegistryEvent.Action.PUSH && tag != null) {
 
                     this.broadcastTrainAvailable(
                             UUID.fromString(target.getRepository()),
                             event.getRequest().getHost(),
-							new URI(event.getRequest().getAddr()));
+							tag);
                 }
 
-			} catch(final IllegalArgumentException | URISyntaxException e) {
+			} catch(final IllegalArgumentException e) {
 
 				e.printStackTrace();
 			    // TODO Currently ignore DockerRegistry events that do not belong to trains
